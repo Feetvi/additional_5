9423ba2b94f10967ff62cdc26f2ef807c7569276
Feetvi <razer-key@mail.ru>
2018-02-26 17:28:44 +0300

    my solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var arr = [];
+	var newStr = " ";	
+
+	for (var i = 0; i < bracketsConfig.length; i++) {
+		arr.push(bracketsConfig[i][0] + bracketsConfig[i][1]);			
+	}	
+
+	while (true) {
+		if (newStr.length != str.length) {
+			newStr = str;
+			for (var i = 0; i < arr.length; i++) {
+				str = str.replace(arr[i], '');
+			}	
+		} else {
+			break;
+		}						
+	}
+
+	if (newStr.length == 0) {
+		return true
+	};
+
+	return false; 
 }
+
+//console.log(check('((()))()', [['(', ')']]));
+
+
